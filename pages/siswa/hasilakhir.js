ad2e78978526b0b3fc36f2e3a42066bd66ba0e2d
Arthur <arthurfraim@gmail.com>
2022-11-23 12:06:44 +0800

    add rapor and hasil ujian

--- pages/siswa/hasilakhir.js
+++ pages/siswa/hasilakhir.js
@@ -63,7 +63,7 @@ const HasilAkhir = () => {
                   <path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z" />
                   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
                 </svg>
-                Absensi
+                Absen
               </Link>
               <Link
                 href={"/siswa/rapor"}
@@ -127,7 +127,40 @@ const HasilAkhir = () => {
               </Link>
             </div>
           </div>
-          <div className="col-md-10"></div>
+          <div className="col-md-10 mt-4">
+            <div className="border-top border-bottom border-2 border-dark">
+              <p className="fw-bold">Keputusan: </p>
+              <p>
+                Berdasarkan hasil yang dicapai pada semester 1 dan 2, peserta
+                didik ditetapkan:{" "}
+              </p>
+              <p className=" fs-5 fw-bold">NAIK KE KELAS : XI ( SEBELAS )</p>
+            </div>
+            <div className="my-5 py-5 d-flex justify-content-around">
+              <div>
+                <p>Mengetahui,</p>
+                <p>Wali kelas</p>
+              </div>
+              <div>
+                <p>Mengetahui,</p>
+                <p>Kepala sekolah</p>
+              </div>
+            </div>
+            <div className="my-5  py-5 d-flex justify-content-around border-bottom border-dark">
+              <div className="">
+                <p className="border-bottom border-dark fw-semibold">
+                  Ibu Nur Aulia
+                </p>
+                <p>NIP: 123456789</p>
+              </div>
+              <div>
+                <p className="border-bottom border-dark fw-semibold">
+                  Bpk John Dante
+                </p>
+                <p>NIP: 987654321</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
